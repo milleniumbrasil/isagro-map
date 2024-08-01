75e1470547b0744d944ab9e1045ad1851f63fd77
Malnati <ricardomalnati@gmail.com>
2024-08-01 18:03:51 -0300

    patch

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 export { default as Mapa } from './Mapa';
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+
   </React.StrictMode>
 );
 reportWebVitals();
