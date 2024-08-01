d661b3970de11d03161901ecfd83a262956dc81b
Malnati <ricardomalnati@gmail.com>
2024-08-01 18:02:22 -0300

    patch

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,1 @@
-import { TileLayerConfig } from "./imapa";
-export interface MapaProps {
-    config: TileLayerConfig;
-}
-declare function Mapa({ config }: MapaProps): import("react/jsx-runtime").JSX.Element;
-export default Mapa;
 export { default as Mapa } from './Mapa';
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 export { default as Mapa } from './Mapa';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+reportWebVitals();
